54bc12fbb900027e16f9cf8a44e659c0ea7fbc80
johnmumu <it_changl@126.com>
2017-03-12 20:10:26 +0800

    company

---
+++ src/java/main/com/main/identification/repository/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.main.identification.repository;
+import com.main.identification.model.Company;
+
+public interface CompanyRepository {
+	
+	/**
+	 * @return
+	 */
+	public int insertCompany(Company company);
+
+}
