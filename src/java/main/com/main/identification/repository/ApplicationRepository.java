6b1219e4ff58a794194ea5ab2fd5fed94e19bd82
johnmumu <it_changl@126.com>
2017-03-12 20:36:58 +0800

    crt

---
+++ src/java/main/com/main/identification/repository/ApplicationRepository.java
@@ -0,0 +1,11 @@
+package com.main.identification.repository;
+import com.main.identification.model.Application;
+
+public interface ApplicationRepository {
+	
+	/**
+	 * @return
+	 */
+	public int insertApplication(Application application);
+
+}
---
+++ src/java/main/com/main/identification/repository/ReportRepository.java
@@ -0,0 +1,11 @@
+package com.main.identification.repository;
+import com.main.identification.model.Report;
+
+public interface ReportRepository {
+	
+	/**
+	 * @return
+	 */
+	public int insertReport(Report report);
+
+}
